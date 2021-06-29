747a4580cc72ab4b5b2bc4ec5e21ff0cf4eb9022
FormerSoldier <793557590@qq.com>
2021-06-29 22:35:51 +0800

    move percent as Order's field

--- src/main/java/com/tw/academy/basic/$7_long_method/Order.java
+++ src/main/java/com/tw/academy/basic/$7_long_method/Order.java
@@ -3,14 +3,16 @@ package com.tw.academy.basic.$7_long_method;
 import java.util.List;
 
 public class Order {
+    double percent;
     String customerName;
     String address;
     List<LineItem> lineItems;
 
-    public Order(String customerName, String address, List<LineItem> lineItems) {
+    public Order(String customerName, String address, List<LineItem> lineItems, double percent) {
         this.customerName = customerName;
         this.address = address;
         this.lineItems = lineItems;
+        this.percent = percent;
     }
 
     public String getCustomerName() {
@@ -28,7 +30,7 @@ public class Order {
     double calculateAllTotalAmount() {
         double allTotalAmount = 0d;
         for (LineItem lineItem : getLineItems()) {
-            allTotalAmount += lineItem.totalAmount() + lineItem.totalAmount() * .10;
+            allTotalAmount += lineItem.totalAmount() + lineItem.totalAmount() * percent;
         }
         return allTotalAmount;
     }
@@ -36,7 +38,7 @@ public class Order {
     double calculateTotSalesTax() {
         double totSalesTax = 0d;
         for (LineItem lineItem : getLineItems()) {
-            totSalesTax += lineItem.totalAmount() * .10;
+            totSalesTax += lineItem.totalAmount() * percent;
         }
         return totSalesTax;
     }
